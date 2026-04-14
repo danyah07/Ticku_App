3306a392d95454bbb8164c2320a4abcfb43369de
danyah07 <danyahal660@gmail.com>
2026-04-14 15:23:53 +0300

    Initial Commit

---
+++ firebasetrial/firebasetrialApp.swift
@@ -0,0 +1,17 @@
+//
+//  firebasetrialApp.swift
+//  firebasetrial
+//
+//  Created by Danyah ALbarqawi on 14/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct firebasetrialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

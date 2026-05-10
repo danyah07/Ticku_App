95f71fb4e4adf5d3767b8e17f39fc2a8bb17bd97
danyah07 <danyahal660@gmail.com>
2026-05-10 16:57:35 +0300

    Adding Files

---
+++ Ticku/Modules/Intro/ViewModels/SplashViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  SplashViewModel.swift
+//  firebasetrial
+//
+//  Created by Danyah ALbarqawi on 10/05/2026.
+//
+
---
+++ Ticku/Modules/Intro/Views/IntroView.swift
@@ -0,0 +1,7 @@
+//
+//  IntroView.swift
+//  firebasetrial
+//
+//  Created by Danyah ALbarqawi on 10/05/2026.
+//
+
